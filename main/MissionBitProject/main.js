8e6ca8fb531f371c255fa1e9a0de7ea2a31dec9f
Kevin <keruizsanchez@gmail.com>
2020-02-19 12:37:42 -0800

    File path fix

--- main/MissionBitProject/main.js
+++ main/MissionBitProject/main.js
@@ -2,18 +2,18 @@ var mainState = {
     preload: function () {
         // Here we preload the assets
 
-        //name image on the left, give path to image on the right 
-        game.load.image('player', 'media/doggo.png');
-        game.load.image('wall', 'media/wall.png');
-        game.load.image('coke', 'media/coke.png');
-        game.load.image('enemy', 'media/depression.png');
-        game.load.image('chef', 'media/chef.png');
-        game.load.image('egg', 'media/egg.png');
-        game.load.image('background', 'media/openconceptkitchen.png')
-        game.load.audio('audio', 'media/Coin.m4a');
-        game.load.audio('die','media/Death.wav');
-        game.load.audio('chef','media/chef.m4a');
-        game.load.audio('jump','media/Jump.wav');
+        //name image on the left, give path to image on the right
+        game.load.image('player', '/media/doggo.png');
+        game.load.image('wall', '/media/wall.png');
+        game.load.image('coke', '/media/coke.png');
+        game.load.image('enemy', '/media/depression.png');
+        game.load.image('chef', '/media/chef.png');
+        game.load.image('egg', '/media/egg.png');
+        game.load.image('background', '/media/openconceptkitchen.png')
+        game.load.audio('audio', '/media/Coin.m4a');
+        game.load.audio('die','/media/Death.wav');
+        game.load.audio('chef','/media/chef.m4a');
+        game.load.audio('jump','/media/Jump.wav');
 
 
     },
@@ -35,11 +35,11 @@ var mainState = {
         // Create the player in the middle of the game
         this.player = game.add.sprite(70, 100, 'player');
         this.score = 0;
-        
+
         game.world.setBounds(0, 0, 3200, 1400)
-        
+
         game.camera.follow(this.player);
-        
+
         // Add gravity to make it fall
         this.player.body.gravity.y = 1000;
 
@@ -48,17 +48,17 @@ var mainState = {
         this.cokes = game.add.group();
         this.enemies = game.add.group();
         this.chefs = game.add.group();
-        
+
         this.danger = game.add.sprite(200, 200, 'chef')
         this.enemies.add(this.danger);
         this.tween = game.add.tween(this.danger).to( { x:400, y:200 }, 2000, "Linear", true, 0, -1);
         this.tween.yoyo(true, 0);
-        
+
         this.danger = game.add.sprite(600, 400, 'chef')
         this.enemies.add(this.danger);
         this.tween = game.add.tween(this.danger).to( { x:800, y:400 }, 2000, "Linear", true, 0, -1);
         this.tween.yoyo(true, 0);
-        
+
         // Design the level. x = wall, o = coke, ! = depression.
         var level = [
             'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
@@ -67,7 +67,7 @@ var mainState = {
             'x                    o                                                    x',
             'x                                    x                  o                 x',
             'x                   xxx            !   !                                  x',
-            'xxxxxxxxxxxxx     xx   xx    xxxx!!!!!!!!!xxxx !! x !x !x !x !x !x !x     x', 
+            'xxxxxxxxxxxxx     xx   xx    xxxx!!!!!!!!!xxxx !! x !x !x !x !x !x !x     x',
             'x             !!!         !!                                              x',
             'x                                                                        xx',
             'x                                                              o        x x',
@@ -88,7 +88,7 @@ var mainState = {
             'x        x x x x x x x x x x x                                            x',
             'x                                                                 x  x   xx',
             'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
-            
+
         ];
 
         // Create the level by going through the array
@@ -115,7 +115,7 @@ var mainState = {
 
 
             }
-            
+
         }
 
     },
@@ -129,9 +129,9 @@ var mainState = {
 
         //check for player and enemy overlapping
         game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
-        
+
         game.physics.arcade.overlap(this.player, this.chefs, this.cook, null, this);
-        
+
 
         if (this.score >= 5) {
             var text = game.add.text(700, 400, "You have stabilized your glucose levels!",
@@ -176,7 +176,7 @@ var mainState = {
                 });
             text.anchor.setTo(0.5, 0.5);
     }
-    
+
 
 };
 
